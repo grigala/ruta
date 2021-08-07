07a7340355dfde3436118e67b7f861c170ca46f4
George Grigalashvili <grigalaa@gmail.com>
2021-08-07 23:31:36 +0200

    disable linter at this point

--- src/main/kotlin/io/github/grigala/plugins/ruta/lang/Icons.kt
+++ src/main/kotlin/io/github/grigala/plugins/ruta/lang/Icons.kt
@@ -3,8 +3,6 @@ package io.github.grigala.plugins.ruta.lang
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
-class Icons {
-    companion object {
-        val APP_ICON: Icon = IconLoader.getIcon("/icons/ruta16x16.svg", Icons::class.java)
-    }
+object Icons {
+    val APP_ICON: Icon = IconLoader.getIcon("/icons/ruta16x16.svg", Icons::class.java)
 }
